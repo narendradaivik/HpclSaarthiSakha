37236709ba9de6f25a5a8253f72a599c71b1568f
Narendra Kumar <nd22982@gmail.com>
2026-03-17 15:20:24 +0530

    enable Signed auto release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,12 +29,21 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+     signingConfigs {
+        release {
+            storeFile file("androiddemo.keystore")
+            storePassword System.getenv("KEYSTORE_PASSWORD")
+            keyAlias System.getenv("KEY_ALIAS")
+            keyPassword System.getenv("KEY_PASSWORD")
+        }
+    }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig signingConfigs.release
+            minifyEnabled false
         }
     }
 }
